a2dbcc572d28d85e2335221a2216d5a2db869ffe
Jesse Shawl <jesse@jesse.sh>
2024-05-09 19:23:22 -0500

    prints color

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os/exec"
+)
 
+type Diff struct {
+}
+
+func main() {
+	cmd := exec.Command("git", "diff", "--color")
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Print the output
+	fmt.Println(string(stdout))
 }
