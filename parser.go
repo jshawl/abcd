879b12cf376e39459581f562547d81203c3cdd0d
Jesse Shawl <jesse@jesse.sh>
2024-05-10 18:55:06 -0500

    parse diff

--- parser.go
+++ parser.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"regexp"
+	"strings"
 )
 
 type Diff struct {
-	Files []string
+	Files []File
+}
+
+type File struct {
+	Name   string
+	Blocks []Block
+}
+
+type Block struct {
+	OldRange string
+	NewRange string
 }
 
 func parseFile(line string) (string, error) {
@@ -18,8 +30,30 @@ func parseFile(line string) (string, error) {
 	return matches[0][1], nil
 }
 
-// func parse(diff string) Diff {
-// 	re := regexp.MustCompile("^diff")
-// 	output := re.Split(string(diff), -1)
-// 	return Diff{Files: []string{""}}
-// }
+func parseBlock(line string) (Block, error) {
+	r := regexp.MustCompile(`@@ -([0-9]+,[0-9]+) \+([0-9]+,?[0-9]*) @@`)
+	matches := r.FindAllStringSubmatch(line, -1)
+	if len(matches) != 1 {
+		return Block{}, errors.New("match not found")
+	}
+	oldRange := matches[0][1]
+	newRange := matches[0][2]
+	return Block{OldRange: oldRange, NewRange: newRange}, nil
+}
+
+func parseDiff(lines string) (Diff, error) {
+	diff := Diff{}
+	var parsedLines []string
+	sc := bufio.NewScanner(strings.NewReader(lines))
+	for sc.Scan() {
+		parsedLines = append(parsedLines, sc.Text())
+	}
+	for _, v := range parsedLines {
+		parsedFile, _ := parseFile(v)
+		if parsedFile != "" {
+			file := File{Name: parsedFile}
+			diff.Files = append(diff.Files, file)
+		}
+	}
+	return diff, nil
+}
--- parser_test.go
+++ parser_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -25,5 +26,36 @@ func TestParseFileOnNonFile(t *testing.T) {
 	_, err := parseFile("not a line with a filename")
 	if err == nil {
 		t.Fatalf("Expected error, got match")
+	}
+}
+
+func TestParseBlockWithAddedLines(t *testing.T) {
+	actual, _ := parseBlock("@@ -0,0 +1 @@")
+	if actual.OldRange != "0,0" {
+		t.Fatalf("Expected old range to be '0,0'")
+	}
+	if actual.NewRange != "1" {
+		t.Fatalf("Expected new range to be '1'")
+	}
+}
+
+func TestParseBlockWithChangedLines(t *testing.T) {
+	actual, _ := parseBlock("@@ -1,4 +1,4 @@")
+	if actual.OldRange != "1,4" {
+		t.Fatalf("Expected old range to be '1,4'")
+	}
+	if actual.NewRange != "1,4" {
+		t.Fatalf("Expected new range to be '1,4'")
+	}
+}
+
+func TestParseDiff(t *testing.T) {
+	contents, _ := os.ReadFile("./test/one-file-one-block.diff")
+	actual, _ := parseDiff(string(contents))
+	if len(actual.Files) != 1 {
+		t.Fatalf("Expected 1 file")
+	}
+	if actual.Files[0].Name != "file" {
+		t.Fatalf("Expected 1 file name to be 'file'")
 	}
 }
