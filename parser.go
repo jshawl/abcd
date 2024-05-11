7a621c55422a17f3472900d9ed7366367b971bd4
Jesse Shawl <jesse@jesse.sh>
2024-05-10 20:00:31 -0500

    parse diff (#2)

--- parser.go
+++ parser.go
@@ -1,25 +1,80 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"regexp"
+	"strings"
 )
 
 type Diff struct {
-	Files []string
+	Files []File
 }
 
-func parseFile(line string) (string, error) {
+type File struct {
+	Name   string
+	Blocks []Block
+}
+
+type Block struct {
+	OldRange string
+	NewRange string
+	Lines    []string
+}
+
+func parseLine(line string) (string, error) {
+	r := regexp.MustCompile(`^(diff|index|---|\+\+\+|@@)`)
+	if !r.MatchString(line) {
+		return line, nil
+	}
+	return "", nil
+}
+
+func parseBlock(line string) (Block, error) {
+	r := regexp.MustCompile(`@@ -([0-9]+,[0-9]+) \+([0-9]+,?[0-9]*) @@`)
+	matches := r.FindAllStringSubmatch(line, -1)
+	if len(matches) != 1 {
+		return Block{}, errors.New("match not found")
+	}
+	oldRange := matches[0][1]
+	newRange := matches[0][2]
+	return Block{OldRange: oldRange, NewRange: newRange}, nil
+}
+
+func parseFile(line string) (File, error) {
 	r := regexp.MustCompile(`diff --git a/([\w\.]+) b/`)
 	matches := r.FindAllStringSubmatch(line, -1)
 	if len(matches) != 1 {
-		return "", errors.New("match not found")
+		return File{}, errors.New("match not found")
 	}
-	return matches[0][1], nil
+	return File{Name: matches[0][1]}, nil
 }
 
-// func parse(diff string) Diff {
-// 	re := regexp.MustCompile("^diff")
-// 	output := re.Split(string(diff), -1)
-// 	return Diff{Files: []string{""}}
-// }
+func parseDiff(lines string) (Diff, error) {
+	diff := Diff{}
+	var parsedLines []string
+	sc := bufio.NewScanner(strings.NewReader(lines))
+	for sc.Scan() {
+		parsedLines = append(parsedLines, sc.Text())
+	}
+	for _, v := range parsedLines {
+		file, _ := parseFile(v)
+		if file.Name != "" {
+			diff.Files = append(diff.Files, file)
+		}
+		lastFile := &diff.Files[len(diff.Files)-1]
+
+		block, _ := parseBlock(v)
+		if block.OldRange != "" {
+			lastFile.Blocks = append(lastFile.Blocks, block)
+		}
+
+		blocks := lastFile.Blocks
+
+		line, _ := parseLine(v)
+		if line != "" {
+			blocks[len(blocks)-1].Lines = append(blocks[len(blocks)-1].Lines, line)
+		}
+	}
+	return diff, nil
+}
--- parser_test.go
+++ parser_test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
 func TestParseFile(t *testing.T) {
 	expected := "file"
 	actual, _ := parseFile("diff --git a/file b/file")
-	if expected != actual {
+	if expected != actual.Name {
 		t.Fatalf(fmt.Sprintf("Expected: %s Actual: %s", expected, actual))
 	}
 }
@@ -16,7 +17,7 @@ func TestParseFile(t *testing.T) {
 func TestParseFileWithExtension(t *testing.T) {
 	expected := "file.txt"
 	actual, _ := parseFile("diff --git a/file.txt b/file.txt")
-	if expected != actual {
+	if expected != actual.Name {
 		t.Fatalf(fmt.Sprintf("Expected: %s Actual: %s", expected, actual))
 	}
 }
@@ -25,5 +26,81 @@ func TestParseFileOnNonFile(t *testing.T) {
 	_, err := parseFile("not a line with a filename")
 	if err == nil {
 		t.Fatalf("Expected error, got match")
+	}
+}
+
+func TestParseBlockWithAddedLines(t *testing.T) {
+	actual, _ := parseBlock("@@ -0,0 +1 @@")
+	if actual.OldRange != "0,0" {
+		t.Fatalf("Expected old range to be '0,0'")
+	}
+	if actual.NewRange != "1" {
+		t.Fatalf("Expected new range to be '1'")
+	}
+}
+
+func TestParseBlockWithChangedLines(t *testing.T) {
+	actual, _ := parseBlock("@@ -1,4 +1,4 @@")
+	if actual.OldRange != "1,4" {
+		t.Fatalf("Expected old range to be '1,4'")
+	}
+	if actual.NewRange != "1,4" {
+		t.Fatalf("Expected new range to be '1,4'")
+	}
+}
+
+func TestParseLineDiffPreamble(t *testing.T) {
+	actual, _ := parseLine("diff --git a/file b/file")
+	if actual != "" {
+		t.Fatalf("Expected diff line to be ignored")
+	}
+}
+
+func TestParseLineIndexPreamble(t *testing.T) {
+	actual, _ := parseLine("index e69de29..d00491f 100644")
+	if actual != "" {
+		t.Fatalf("Expected index line to be ignored")
+	}
+}
+
+func TestParseLineOldPreamble(t *testing.T) {
+	actual, _ := parseLine("--- a/file")
+	if actual != "" {
+		t.Fatalf("Expected old line to be ignored")
+	}
+}
+
+func TestParseLineBlockPreamble(t *testing.T) {
+	actual, _ := parseLine("@@ -0,0 +1 @@")
+	if actual != "" {
+		t.Fatalf("Expected block line to be ignored")
+	}
+}
+
+func TestParseLineDiff(t *testing.T) {
+	actual, _ := parseLine("- removed this line")
+	if actual != "- removed this line" {
+		t.Fatalf("Expected line to be parsed")
+	}
+}
+
+func TestParseLineNewPreamble(t *testing.T) {
+	actual, _ := parseLine("+++ a/file")
+	if actual != "" {
+		t.Fatalf("Expected new line to be ignored")
+	}
+}
+
+func TestParseDiff(t *testing.T) {
+	contents, _ := os.ReadFile("./test/one-file-one-block.diff")
+	actual, _ := parseDiff(string(contents))
+	if len(actual.Files) != 1 {
+		t.Fatalf("Expected 1 file")
+	}
+	if actual.Files[0].Name != "file" {
+		t.Fatalf("Expected 1 file name to be 'file'")
+	}
+	if len(actual.Files[0].Blocks[0].Lines) != 1 {
+		t.Fatalf("Expected 1 File, 1 Block, 1 Line")
 	}
 }
