cee4e12a40a19fc0010f9678d2d6881cadbf27aa
Jesse Shawl <jesse@jesse.sh>
2024-05-20 05:51:46 -0500

    add line numbers

--- ui/file.go
+++ ui/file.go
@@ -1,8 +1,11 @@
 package ui
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/jshawl/abcd/parser"
 )
 
@@ -22,17 +25,34 @@ func NewFile(file parser.File) File {
 	}
 }
 
+var lineNumberStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#aaa"))
+
 func (m File) View(viewportWidth int) string {
 	var content strings.Builder
 	content.WriteString(fileStyle.Width(viewportWidth).Render(m.Name))
 	content.WriteString("\n")
 	for blockI, block := range m.Blocks {
-		for _, line := range block.Lines {
+		newStart := strings.Split(block.NewRange, ",")
+		newLineCounter, _ := strconv.Atoi(newStart[0])
+		oldStart := strings.Split(block.OldRange, ",")
+		oldLineCounter, _ := strconv.Atoi(oldStart[0])
+		for i, line := range block.Lines {
+
 			if strings.HasPrefix(line, "-") {
-				content.WriteString(removedStyle.Width(viewportWidth).Render(line))
+				lineNumber := lineNumberStyle.Render(fmt.Sprintf("%d ", i+oldLineCounter))
+				width := viewportWidth - lipgloss.Width(lineNumber)
+				content.WriteString(lineNumber)
+				newLineCounter--
+				content.WriteString(removedStyle.Width(width).Render(line))
 			} else if strings.HasPrefix(line, "+") {
-				content.WriteString(addedStyle.Width(viewportWidth).Render(line))
+				lineNumber := lineNumberStyle.Render(fmt.Sprintf("%d ", i+newLineCounter))
+				width := viewportWidth - lipgloss.Width(lineNumber)
+				content.WriteString(lineNumber)
+				oldLineCounter--
+				content.WriteString(addedStyle.Width(width).Render(line))
 			} else {
+				lineNumber := lineNumberStyle.Render(fmt.Sprintf("%d ", i+newLineCounter))
+				content.WriteString(lineNumber)
 				content.WriteString(line)
 			}
 			content.WriteString("\n")
